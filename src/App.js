c2f2f17bcebc5e21b2b9357bdc6e27865776db30
codeshootah <vnukovg@gmail.com>
2019-10-24 21:57:18 +0300

    remove bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.scss';
-import  { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NoMatch from './pages/NoMatch';
-import { Container } from 'react-bootstrap';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <React.Fragment>
-      <NavigationBar />
-      <Container>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/about" component={ About } />
-            <Route exact path="/contact" component={ Contact } />
-            <Route component={ NoMatch } />
-          </Switch>
-        </Router>
-      </Container>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/about" component={ About } />
+          <Route exact path="/contact" component={ Contact } />
+          <Route component={ NoMatch } />
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 }
