dfc7a2de156190d3455a27ebb6e18c6708d16b65
codeshootah <vnukovg@gmail.com>
2019-10-24 16:06:50 +0300

    add router and bootstrap

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, itaque.</p>
+    </div>
+  )
+}
---
+++ src/pages/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NoMatch() {
+  return (
+    <div>
+      <h2>Error 404</h2>
+    </div>
+  )
+}
