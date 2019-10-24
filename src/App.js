872166af0cf5123ab0593877dd4f230066eecf03
codeshootah <vnukovg@gmail.com>
2019-10-24 21:11:58 +0300

    made navbar component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NoMatch from './pages/NoMatch';
-
+import { Container } from 'react-bootstrap';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
+      <NavigationBar />
+      <Container>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/about" component={ About } />
+            <Route exact path="/contact" component={ Contact } />
+            <Route component={ NoMatch } />
+          </Switch>
+        </Router>
+      </Container>
     </React.Fragment>
   );
 }
